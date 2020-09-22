e079ca10fb04a1723943731a31290a446b7f1b37
Meera Patel <patelmeeray@gmail.com>
2020-09-22 00:37:55 -0400

    automated data refresh

--- R/data.R
+++ R/data.R
@@ -2,15 +2,15 @@
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 7772 rows and 10 variables:
 #' \describe{
-#'   \item{\code{concept_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{domain_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{vocabulary_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_class_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{standard_concept}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_code}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_start_date}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{valid_end_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{valid_start_date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valid_end_date}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{invalid_reason}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
@@ -20,20 +20,34 @@
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 7804 rows and 3 variables:
 #' \describe{
-#'   \item{\code{concept_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_definition}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "CONCEPT_DEFINITION"
 
+#' @title CONCEPT_RELATIONSHIP
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 41131 rows and 6 variables:
+#' \describe{
+#'   \item{\code{concept_id_1}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id_2}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{relationship_id}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valid_start_date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valid_end_date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{invalid_reason}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"CONCEPT_RELATIONSHIP"
+
 #' @title CONCEPT_SYNONYM
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 35881 rows and 3 variables:
 #' \describe{
-#'   \item{\code{concept_id}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_synonym_name}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{language_concept_id}}{double COLUMN_DESCRIPTION} 
+#'   \item{\code{language_concept_id}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "CONCEPT_SYNONYM"
@@ -48,6 +62,16 @@
 #'}
 #' @details DETAILS
 "DRUG_DICTIONARY"
+
+#' @title DRUG_DICTIONARY_LOG
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 2 rows and 2 variables:
+#' \describe{
+#'   \item{\code{ddl_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_count}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"DRUG_DICTIONARY_LOG"
 
 #' @title DRUG_LINK
 #' @description DATASET_DESCRIPTION
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,9 +2,11 @@ library(broca)
 
 CONCEPT <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//CONCEPT.csv')
 CONCEPT_DEFINITION <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//CONCEPT_DEFINITION.csv')
+CONCEPT_RELATIONSHIP <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//CONCEPT_RELATIONSHIP.csv')
 CONCEPT_SYNONYM <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//CONCEPT_SYNONYM.csv')
 DRUG_DICTIONARY <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//DRUG_DICTIONARY.csv')
+DRUG_DICTIONARY_LOG <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//DRUG_DICTIONARY_LOG.csv')
 DRUG_LINK <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//DRUG_LINK.csv')
 DRUG_LINK_SYNONYM <- broca::simply_read_csv('/Users/meerapatel/GitHub/cancergovData//data-raw//DRUG_LINK_SYNONYM.csv')
 
-usethis::use_data(CONCEPT,CONCEPT_DEFINITION,CONCEPT_SYNONYM,DRUG_DICTIONARY,DRUG_LINK,DRUG_LINK_SYNONYM, overwrite = TRUE)
+usethis::use_data(CONCEPT,CONCEPT_DEFINITION,CONCEPT_RELATIONSHIP,CONCEPT_SYNONYM,DRUG_DICTIONARY,DRUG_DICTIONARY_LOG,DRUG_LINK,DRUG_LINK_SYNONYM, overwrite = TRUE)
