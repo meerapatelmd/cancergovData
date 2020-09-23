088b6f984ada1956d9e199a37de4335b17a042f2
Meera Patel <patelmeeray@gmail.com>
2020-09-23 03:11:15 -0400

    automated data refresh

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' @title CONCEPT
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 7772 rows and 10 variables:
+#' @format A data frame with 7777 rows and 10 variables:
 #' \describe{
 #'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
@@ -18,7 +18,7 @@
 
 #' @title CONCEPT_DEFINITION
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 7804 rows and 3 variables:
+#' @format A data frame with 7809 rows and 3 variables:
 #' \describe{
 #'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
@@ -43,7 +43,7 @@
 
 #' @title CONCEPT_SYNONYM
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 35881 rows and 3 variables:
+#' @format A data frame with 35901 rows and 3 variables:
 #' \describe{
 #'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_synonym_name}}{character COLUMN_DESCRIPTION}
