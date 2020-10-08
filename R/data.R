34aa4b37bf331bded1f03a961541c873c402c956
Meera <patelmeeray@gmail.com>
2020-10-07 20:54:07 -0400

    update data package

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' @title DRUG_DICTIONARY
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 7773 rows and 3 variables:
+#' @format A data frame with 7776 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dd_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug}}{character COLUMN_DESCRIPTION}
@@ -11,7 +11,7 @@
 
 #' @title DRUG_DICTIONARY_LOG
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 6 rows and 2 variables:
+#' @format A data frame with 8 rows and 2 variables:
 #' \describe{
 #'   \item{\code{ddl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_count}}{double COLUMN_DESCRIPTION} 
@@ -21,7 +21,7 @@
 
 #' @title DRUG_LINK
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 7782 rows and 3 variables:
+#' @format A data frame with 7785 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug}}{character COLUMN_DESCRIPTION}
@@ -32,12 +32,10 @@
 
 #' @title DRUG_LINK_SYNONYM
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 37439 rows and 6 variables:
+#' @format A data frame with 18944 rows and 4 variables:
 #' \describe{
 #'   \item{\code{dls_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{x1}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{x2}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{drug_synonym_type}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{drug_synonym}}{character COLUMN_DESCRIPTION} 
 #'}
@@ -46,7 +44,7 @@
 
 #' @title DRUG_LINK_URL
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 1644 rows and 3 variables:
+#' @format A data frame with 11842 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dlu_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
@@ -57,7 +55,7 @@
 
 #' @title DRUG_LINK_NCIT
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 8 rows and 3 variables:
+#' @format A data frame with 5842 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dln_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
@@ -65,3 +63,18 @@
 #'}
 #' @details DETAILS
 "DRUG_LINK_NCIT"
+
+#' @title NCIT_SYNONYM
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 34603 rows and 7 variables:
+#' \describe{
+#'   \item{\code{ns_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{ncit_code}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{ncit_code_url}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{term}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{source}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{tty}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{code}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"NCIT_SYNONYM"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,11 @@
 library(readr)
-DRUG_DICTIONARY <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_DICTIONARY.csv')
-DRUG_DICTIONARY_LOG <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_DICTIONARY_LOG.csv')
-DRUG_LINK <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK.csv')
-DRUG_LINK_SYNONYM <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_SYNONYM.csv')
-DRUG_LINK_URL <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_URL.csv')
-DRUG_LINK_NCIT <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_NCIT.csv')
+DRUG_DICTIONARY <- readr::read_csv('data-raw/DRUG_DICTIONARY.csv')
+DRUG_DICTIONARY_LOG <- readr::read_csv('data-raw/DRUG_DICTIONARY_LOG.csv')
+DRUG_LINK <- readr::read_csv('data-raw/DRUG_LINK.csv')
+DRUG_LINK_SYNONYM <- readr::read_csv('data-raw/DRUG_LINK_SYNONYM.csv')
+DRUG_LINK_URL <- readr::read_csv('data-raw/DRUG_LINK_URL.csv')
+DRUG_LINK_NCIT <- readr::read_csv('data-raw/DRUG_LINK_NCIT.csv')
+NCIT_SYNONYM <- readr::read_csv('data-raw/NCIT_SYNONYM.csv')
 
 usethis::use_data(
 	DRUG_DICTIONARY,
@@ -12,5 +13,6 @@ usethis::use_data(
 	DRUG_LINK,
 	DRUG_LINK_SYNONYM,
 	DRUG_LINK_URL,
-	DRUG_LINK_NCIT
+	DRUG_LINK_NCIT,
+	NCIT_SYNONYM
 , overwrite = TRUE)
