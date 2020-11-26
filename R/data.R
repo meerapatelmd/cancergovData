4e0c3d67ad17ac3f8ff5fc01ad33ca60c1893d58
Meera <patelm9@mskcc.org>
2020-11-26 02:02:09 -0500

    update documentation

--- R/data.R
+++ R/data.R
@@ -1,38 +1,51 @@
+#' @title CANCERGOV_DRUG
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 66456 rows and 5 variables:
+#' \describe{
+#'   \item{\code{cd_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{ncit_code}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{drug_synonym_type}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{cancergov_drug}}{character COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"CANCERGOV_DRUG" 
+
+#' @title DRUG_DICTIONARY_LOG
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{\code{ddl_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_count}}{double COLUMN_DESCRIPTION} 
+#'}
+#' @details DETAILS
+"DRUG_DICTIONARY_LOG" 
+
 #' @title DRUG_DICTIONARY
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 8156 rows and 3 variables:
+#' @format A data frame with 8457 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dd_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{definition}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"DRUG_DICTIONARY"
+"DRUG_DICTIONARY" 
 
-#' @title DRUG_DICTIONARY_LOG
+#' @title DRUG_LINK_NCIT
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 12 rows and 2 variables:
+#' @format A data frame with 7981 rows and 3 variables:
 #' \describe{
-#'   \item{\code{ddl_datetime}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{drug_count}}{double COLUMN_DESCRIPTION} 
+#'   \item{\code{dln_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{ncit_code}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"DRUG_DICTIONARY_LOG"
-
-#' @title DRUG_LINK
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 10115 rows and 3 variables:
-#' \describe{
-#'   \item{\code{dl_datetime}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{drug}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION} 
-#'}
-#' @details DETAILS
-"DRUG_LINK"
+"DRUG_LINK_NCIT" 
 
 #' @title DRUG_LINK_SYNONYM
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 26224 rows and 4 variables:
+#' @format A data frame with 46094 rows and 4 variables:
 #' \describe{
 #'   \item{\code{dls_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
@@ -40,33 +53,33 @@
 #'   \item{\code{drug_synonym}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"DRUG_LINK_SYNONYM"
+"DRUG_LINK_SYNONYM" 
 
 #' @title DRUG_LINK_URL
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 15837 rows and 3 variables:
+#' @format A data frame with 19374 rows and 3 variables:
 #' \describe{
 #'   \item{\code{dlu_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{drug_link_url}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"DRUG_LINK_URL"
+"DRUG_LINK_URL" 
 
-#' @title DRUG_LINK_NCIT
+#' @title DRUG_LINK
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 7792 rows and 3 variables:
+#' @format A data frame with 10378 rows and 3 variables:
 #' \describe{
-#'   \item{\code{dln_datetime}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{ncit_code}}{character COLUMN_DESCRIPTION} 
+#'   \item{\code{dl_datetime}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{drug}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{drug_link}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"DRUG_LINK_NCIT"
+"DRUG_LINK" 
 
 #' @title NCIT_SYNONYM
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 50018 rows and 7 variables:
+#' @format A data frame with 36123 rows and 7 variables:
 #' \describe{
 #'   \item{\code{ns_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{ncit_code}}{character COLUMN_DESCRIPTION}
@@ -77,4 +90,5 @@
 #'   \item{\code{code}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
-"NCIT_SYNONYM"
+"NCIT_SYNONYM" 
+
---
+++ data-raw/usethis.R
@@ -0,0 +1,20 @@
+library(readr)
+CANCERGOV_DRUG <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/CANCERGOV_DRUG.csv")
+DRUG_DICTIONARY_LOG <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_DICTIONARY_LOG.csv")
+DRUG_DICTIONARY <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_DICTIONARY.csv")
+DRUG_LINK_NCIT <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_NCIT.csv")
+DRUG_LINK_SYNONYM <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_SYNONYM.csv")
+DRUG_LINK_URL <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK_URL.csv")
+DRUG_LINK <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/DRUG_LINK.csv")
+NCIT_SYNONYM <- readr::read_csv("/Users/patelm9/GitHub/Public-Packages/cancergovData/data-raw/NCIT_SYNONYM.csv")
+usethis::use_data(
+	CANCERGOV_DRUG,
+	DRUG_DICTIONARY_LOG,
+	DRUG_DICTIONARY,
+	DRUG_LINK_NCIT,
+	DRUG_LINK_SYNONYM,
+	DRUG_LINK_URL,
+	DRUG_LINK,
+	NCIT_SYNONYM,
+overwrite = TRUE
+)
